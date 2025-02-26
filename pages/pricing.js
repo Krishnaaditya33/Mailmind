79e1afd9d8ec1554851dcd9fc5059b9c0fabf1e6
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-02-26 14:28:46 +0530

    Create pricing.js

---
+++ pages/pricing.js
@@ -0,0 +1,8 @@
+export default function Pricing() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-white bg-gray-800">
+      <h1 className="text-4xl font-bold">Pricing</h1>
+      <p className="text-lg mt-4">Choose a plan that fits your needs.</p>
+    </div>
+  );
+}

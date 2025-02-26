f594713ebcd8dc7cf79822b049129ad20ad3ed03
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-02-26 14:29:34 +0530

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-white bg-gray-800">
+      <h1 className="text-4xl font-bold">Contact Us</h1>
+      <p className="text-lg mt-4">Reach out to us for inquiries and support.</p>
+    </div>
+  );
+}

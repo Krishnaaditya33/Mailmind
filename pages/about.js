2cf3b03f65383261f307d03ade6fe8625fae2ff5
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-02-26 14:29:10 +0530

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-white bg-gray-800">
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <p className="text-lg mt-4">Learn more about our startup and vision.</p>
+    </div>
+  );
+}

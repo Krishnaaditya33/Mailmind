1c3136bdcc84195131002322f98cbe29ebda80e5
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-03-04 01:59:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@ module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
-        net: false,
-        tls: false,
-        fs: false,
-        // add any others that might cause issues
+        child_process: false,
       };
     }
     return config;
   },
 };
+

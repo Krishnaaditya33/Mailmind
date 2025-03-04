1f7ef4452d9c2134a1f665e4ab3a136a1baea7c5
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-03-04 08:28:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // Provide fallbacks for Node.js modules that aren’t available in the browser.
       config.resolve.fallback = {
+        net: false,
+        tls: false,
+        fs: false,
         child_process: false,
       };
     }

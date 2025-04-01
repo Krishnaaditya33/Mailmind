ce372b27c6cda61621b0f1422bee941942f9aaf7
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-01 15:35:06 +0530

    Create NavBar.js

---
+++ components/NavBar.js
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full p-4 bg-black text-white flex justify-center space-x-4">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/features">Features</Link>
+      <Link href="/pricing">Pricing</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
+  );
+}

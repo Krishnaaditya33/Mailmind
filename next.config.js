4d894aea699a4db0b9d6cd4cf78873c3aecf3e39
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-02 20:20:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'export', // Generates static files, outputs to 'out'
-  trailingSlash: true, // Adds trailing slashes to URLs
-  rewrites: async () => [], // Empty array for rewrite rules
-  redirects: async () => [], // Empty array for redirect rules
-  headers: async () => [] // Empty array for header rules
+  swcMinify: true, // Optional in Next.js 15, can remove
+  output: 'export',
+  distDir: 'public', // Change output to 'public'
+  trailingSlash: true,
+  rewrites: async () => [],
+  redirects: async () => [],
+  headers: async () => []
 };
-
 module.exports = nextConfig;

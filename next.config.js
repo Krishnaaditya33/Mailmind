5b0c17a62eb3d1321f4967fef98bdb33e2c7f9ce
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-02 15:32:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export', // Generates static files, outputs to 'out'
-  trailingSlash: true, // Boolean
+  trailingSlash: true, // Adds trailing slashes to URLs
   rewrites: async () => [], // Empty array for rewrite rules
   redirects: async () => [], // Empty array for redirect rules
   headers: async () => [] // Empty array for header rules
